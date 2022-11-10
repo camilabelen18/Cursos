7d0d4bb2648f819af6e9af3e9efec700c537fa45
Ivancitoo <ivamaldonado@alumno.unlam.edu.ar>
2022-11-10 16:40:47 -0300

    Update ServicioCarritoTest.java

--- src/test/java/servicios/ServicioCarritoTest.java
+++ src/test/java/servicios/ServicioCarritoTest.java
@@ -1,19 +1,31 @@
 package servicios;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import servicios.ServicioCarrito;
+import modelo.Carrito;
 import modelo.Curso;
+import modelo.DatosRegistro;
 import modelo.Estado;
+import modelo.Usuario;
+import repositorios.RepositorioCarrito;
+import repositorios.RepositorioUsuario;
+import repositorios.RepositorioCurso;
 
 public class ServicioCarritoTest {
-	ServicioCarrito servicioCarrito = mock(ServicioCarrito.class);
+	RepositorioUsuario repositorioUsuario = mock(RepositorioUsuario.class);
+	RepositorioCarrito repositorioCarrito = mock(RepositorioCarrito.class);
+	RepositorioCurso repositorioCurso = mock(RepositorioCurso.class);
+	ServicioCurso servicioCurso=new ServicioCursoImpl(repositorioCurso, repositorioUsuario);
+	ServicioUsuario servicioUsuario = new ServicioUsuarioImpl(repositorioUsuario, repositorioCarrito, repositorioCurso);
+	ServicioCarrito servicioCarrito = new ServicioCarritoImpl(repositorioCarrito, repositorioUsuario, servicioUsuario);
 
 	@Test
 	public void testQueObtengaElTotalDelCarrito() {
@@ -29,7 +41,40 @@ public class ServicioCarritoTest {
 		System.out.println(curso2.getPrecio());
 		System.out.println(total);
 		// Comprobacion
-//		assertSame(2000.0, total);
+		assertEquals(5500.0, total, 0.1);
 
+	}
+	@Test
+	public void testQueObtengaLosCursosDelCarrito() {
+		// Preparacion
+		List<Curso> listaCursos = new ArrayList();
+		Curso curso1 = new Curso("Curso PHP/MySql desde 0", "programacion", "En este curso se vera como crear aplicaciones y sitios web desde cero con PHP y MYSQL.", 3000.0, Estado.EN_VENTA, "php-desde-cero.jpg");
+		Curso curso2 = new Curso("Spring Framework 5", "programacion", "Construye aplicaciones web con Spring Framework 5 & Spring Boot: Thymeleaf, JPA, Security, REST, MySQL, Angular, WebFlux.", 2500.0, Estado.EN_VENTA, "curso-spring.jpg");
+		Carrito carrito = new Carrito();
+		listaCursos.add(curso1);
+		listaCursos.add(curso2);
+		
+		// Ejecucion
+		
+		when(repositorioCarrito.obtenerCursosDelCarrito(carrito)).thenReturn(listaCursos);
+
+		List <Curso> cursosObtenidos = repositorioCarrito.obtenerCursosDelCarrito(carrito);
+		
+		// Comprobacion
+		assertThat(cursosObtenidos).isNotEmpty();
+
+	}
+	
+
+	@Test(expected = ListaCarritoException.class)
+	public void queNoSePuedaObtenerCursosDeUnCarritoVacio() {
+
+		// Preparación
+		Carrito carrito = new Carrito();
+		List<Curso> listaCursosVacia = new ArrayList<>();
+		// Ejecución
+		when(repositorioCarrito.obtenerCursosDelCarrito(carrito)).thenReturn(listaCursosVacia);
+		servicioCarrito.obtenerCursosDelCarrito(carrito);
+		
 	}
 }
