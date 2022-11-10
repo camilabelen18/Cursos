3133dc7530b98358a30a68b7976d65401aba67cc
Ivancitoo <ivamaldonado@alumno.unlam.edu.ar>
2022-11-10 14:55:43 -0300

    Update ControladorCarritoTest.java

--- src/test/java/controladores/ControladorCarritoTest.java
+++ src/test/java/controladores/ControladorCarritoTest.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpSession;
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
+import modelo.Carrito;
+import modelo.Carrito_Curso;
 import modelo.Curso;
 import modelo.DatosCreacionCurso;
 import modelo.Estado;
@@ -33,21 +35,74 @@ public class ControladorCarritoTest {
 	public void testQueAgregaCursoAlCarrito() {
 
 		// Preparacion
-		
+
 		Integer curso_id = 1;
-		
+		Integer user_id = 1;
 		// Ejecucion
-		
-		when(session.getAttribute("idUsuario")).thenReturn(1);
+		when(servicioCurso.buscarCursoPorId(curso_id)).thenReturn(new Curso());
+		when(servicioCarrito.obtenerCarritoPorIdUsuario(user_id)).thenReturn(new Carrito());
+		when(session.getAttribute("idUsuario")).thenReturn(user_id);
 		ModelAndView mav = controladorCarrito.agregarCursoAlCarrito(curso_id, session);
-		
+
 		// Comprobacion
-		
+
 		assertThat(mav.getViewName()).isEqualTo("redirect:/verListaCursos");
 
 	}
-	
-	
-	
+
+	@Test
+	public void testQueElimineCursoDelCarrito() {
+
+		// Preparacion
+
+		Integer curso_id = 1;
+		Integer user_id = 1;
+		Carrito carrito = new Carrito();
+		Curso curso1 = new Curso("Curso php", "Programacion", "Curso de programacion php", 1000.0, Estado.EN_CURSO,
+				"cursophp.png");
+		Carrito_Curso cc = new Carrito_Curso();
+		// Ejecucion
+
+		when(servicioCurso.buscarCursoPorId(curso_id)).thenReturn(curso1);
+		when(servicioCarrito.obtenerCarritoPorIdUsuario(user_id)).thenReturn(carrito);
+		when(servicioCarrito.obtenerCarritoCurso(carrito, curso1)).thenReturn(cc);
+		when(session.getAttribute("idUsuario")).thenReturn(user_id);
+		ModelAndView mav = controladorCarrito.eliminarCursoDeListaCarrito(curso_id, session);
+
+		// Comprobacion
+
+		assertThat(mav.getViewName()).isEqualTo("redirect:/vistaCarrito");
+
+	}
+
+	@Test
+	public void testQueDejeIrACarritoConSesionIniciada() {
+
+		// Preparacion
+		Integer user_id = 1;
+		// Ejecucion
+
+		when(session.getAttribute("idUsuario")).thenReturn(user_id);
+
+		ModelAndView mav = controladorCarrito.vistaCarrito(session);
+
+		// Comprobacion
+
+		assertThat(mav.getViewName()).isEqualTo("carrito");
+	}
+
+	@Test
+	public void testQueNoDejeIrACarritoSinIniciarSesion() {
+
+		Integer user_id = null;
+		// Ejecucion
+
+		when(session.getAttribute("idUsuario")).thenReturn(user_id);
+		ModelAndView mav = controladorCarrito.vistaCarrito(session);
+
+		// Comprobacion
+
+		assertThat(mav.getViewName()).isEqualTo("redirect:/");
+	}
 
 }
