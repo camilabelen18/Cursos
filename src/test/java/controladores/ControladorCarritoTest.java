6692a3fb8913199b1745e919da2bb0fb677c37a9
Ivancitoo <ivamaldonado@alumno.unlam.edu.ar>
2022-11-10 14:35:59 -0300

    .

--- src/test/java/controladores/ControladorCarritoTest.java
+++ src/test/java/controladores/ControladorCarritoTest.java
@@ -1,5 +1,53 @@
 package controladores;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import modelo.Curso;
+import modelo.DatosCreacionCurso;
+import modelo.Estado;
+import modelo.Usuario;
+import servicios.ServicioCarrito;
+import servicios.ServicioCurso;
+import servicios.ServicioUsuario;
+
 public class ControladorCarritoTest {
+
+	ServicioCurso servicioCurso = mock(ServicioCurso.class);
+	ServicioCarrito servicioCarrito = mock(ServicioCarrito.class);
+	ServicioUsuario servicioUsuario = mock(ServicioUsuario.class);
+	ControladorCarrito controladorCarrito = new ControladorCarrito(servicioCurso, servicioCarrito);
+	HttpSession session = mock(HttpSession.class);
+
+	@Test
+	public void testQueAgregaCursoAlCarrito() {
+
+		// Preparacion
+		
+		Integer curso_id = 1;
+		
+		// Ejecucion
+		
+		when(session.getAttribute("idUsuario")).thenReturn(1);
+		ModelAndView mav = controladorCarrito.agregarCursoAlCarrito(curso_id, session);
+		
+		// Comprobacion
+		
+		assertThat(mav.getViewName()).isEqualTo("redirect:/verListaCursos");
+
+	}
+	
+	
+	
 
 }
--- src/test/java/servicios/ServicioCarritoTest.java
+++ src/test/java/servicios/ServicioCarritoTest.java
@@ -1,5 +1,36 @@
 package servicios;
 
-public class ServicioCarritoTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import servicios.ServicioCarrito;
+import modelo.Curso;
+import modelo.Estado;
+
+public class ServicioCarritoTest {
+	ServicioCarrito servicioCarrito = mock(ServicioCarrito.class);
+	
+	@Test
+	public void testQueObtengaElTotalDelCarrito() {
+		//Preparacion
+		Curso curso1 = new Curso("Curso php","Programacion","Curso de programacion php", 1000.0,Estado.EN_CURSO, "cursophp.png");
+		Curso curso2= new Curso("Curso php","Programacion","Curso de programacion php", 1000.0,Estado.EN_CURSO, "cursophp.png");
+		List <Curso> cursos = new ArrayList();
+		cursos.add(curso1);
+		cursos.add(curso2);
+		
+		//Ejecucion
+System.out.println(curso1.getPrecio());
+System.out.println(curso2.getPrecio());
+System.out.println(servicioCarrito.getTotalDePrecios(cursos));
+		//Comprobacion
+//		assertSame(2000.0, total);
+		
+		
+	}
 }
