772964a4dba94a7de0618d99e351f6c524d5f077
Ivancitoo <ivamaldonado@alumno.unlam.edu.ar>
2022-11-24 19:15:13 -0300

    añadida notificacion cuando envias y recibis puntos

--- src/main/java/servicios/ServicioUsuarioImpl.java
+++ src/main/java/servicios/ServicioUsuarioImpl.java
@@ -357,5 +357,13 @@ public class ServicioUsuarioImpl implements ServicioUsuario {
 		}
 
 	}
+	
+	@Override
+	public void enviarNotificacion(Usuario usuario, String msj) {
+
+		Notificacion noti = new Notificacion(msj);
+		repositorioUsuario.guardarNotificacionDelUsuario(noti, usuario);
+		
+	}
 
 }
